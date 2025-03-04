e825f5d163df014ee700623e9d9bda570fb04187
AlexD990 <alexanderdanilovv@gmail.com>
2025-03-04 16:38:01 +0200

    Revert "Change some routes"
    This reverts commit b9827d86963e51ffb6b627c8cd1176f114d10520.

--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -5,8 +5,7 @@ import {
 	getMovieTrailer,
 	getSimilarMovies,
 	getTrendingMovie,
-	getNowPlayingMovies,
-	getRecommendationMovies
+	getNowPlayingMovies
 } from "../controllers/movie.controller.js";
 
 const router = express.Router();
@@ -16,7 +15,6 @@ router.get("/nowplaying", getNowPlayingMovies);
 router.get("/:id/trailer", getMovieTrailer);
 router.get("/:id/details", getMovieDetails);
 router.get("/:id/similar", getSimilarMovies);
-router.get("/:id/recommendations", getRecommendationMovies);
 router.get("/:category", getMoviesByCategory);
 
 export default router;
--- backend/routes/tv.route.js
+++ backend/routes/tv.route.js
@@ -5,9 +5,7 @@ import {
 	getTvDetails,
 	getTvsByCategory,
 	getTvTrailers,
-	getPopularTv,
-	getRecommendationTvs,
-	getTvKeywords
+	getPopularTv
 } from "../controllers/tv.controller.js";
 
 const router = express.Router();
@@ -17,8 +15,6 @@ router.get("/popular", getPopularTv);
 router.get("/:id/trailers", getTvTrailers);
 router.get("/:id/details", getTvDetails);
 router.get("/:id/similar", getSimilarTvs);
-router.get("/:id/recommendations", getRecommendationTvs);
-router.get("/:id/keywords", getTvKeywords);
 router.get("/:category", getTvsByCategory);
 
 export default router;
